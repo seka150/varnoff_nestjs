7835953822f8146f86b2d5a6c558473ae3313ff5
Екатерина Петрова <ekaterinap@MacBook-Air-Ekaterina.local>
2024-01-09 01:29:13 +0400

    init repository

---
+++ src/moks/index.ts
@@ -0,0 +1,180 @@
+export const users = [
+        {
+          "id": "6597dc6cd31c7f5e08d01bf4",
+          "email": "benjamin_curry@retrack.dabur",
+          "roles": [
+            "member"
+          ],
+          "apiKey": "b141a619-bc16-4587-b262-b382b240a48c",
+          "profile": {
+            "dob": "1993-05-09",
+            "name": "Benjamin Curry",
+            "about": "Incididunt ipsum laboris velit adipisicing consectetur aliquip fugiat sunt deserunt sunt irure eiusmod ea. Est amet est quis ipsum elit dolor.",
+            "address": "7 Ryder Avenue, Richville, Massachusetts",
+            "company": "Retrack",
+            "location": {
+              "lat": -56.479466,
+              "long": 73.179011
+            }
+          },
+          "username": "benjamin93",
+          "createdAt": "2014-02-23T09:44:30.833Z",
+          "updatedAt": "2014-02-24T09:44:30.833Z"
+        },
+        {
+          "id": "6597dc6ce8d816641f062a9b",
+          "email": "neva_george@quiltigen.sy",
+          "roles": [
+            "admin"
+          ],
+          "apiKey": "42b40a15-5222-42b9-b097-c69ca2ed6648",
+          "profile": {
+            "dob": "1990-06-06",
+            "name": "Neva George",
+            "about": "Reprehenderit qui officia minim ut exercitation aliqua eiusmod pariatur. Voluptate qui cillum occaecat non.",
+            "address": "20 Guider Avenue, Somerset, South Carolina",
+            "company": "Quiltigen",
+            "location": {
+              "lat": -33.619657,
+              "long": -80.517067
+            }
+          },
+          "username": "neva90",
+          "createdAt": "2012-02-18T19:37:37.515Z",
+          "updatedAt": "2012-02-19T19:37:37.515Z"
+        },
+        {
+          "id": "6597dc6c52d717662775d30a",
+          "email": "magdalena_wyatt@strozen.review",
+          "roles": [
+            "owner"
+          ],
+          "apiKey": "4350b4f5-7fc6-40c5-9c9f-5569e5ae8711",
+          "profile": {
+            "dob": "1993-01-12",
+            "name": "Magdalena Wyatt",
+            "about": "Nisi elit et veniam ut aliqua magna aute aliqua excepteur minim. Duis incididunt eu exercitation ex quis sit est.",
+            "address": "44 Hendrix Street, Joppa, Puerto Rico",
+            "company": "Strozen",
+            "location": {
+              "lat": -59.091308,
+              "long": 155.062287
+            }
+          },
+          "username": "magdalena93",
+          "createdAt": "2014-08-24T18:38:10.952Z",
+          "updatedAt": "2014-08-25T18:38:10.952Z"
+        },
+        {
+          "id": "6597dc6c4abec504e4f0fd1e",
+          "email": "mandy_durham@zizzle.email",
+          "roles": [
+            "guest"
+          ],
+          "apiKey": "f70e8cec-b347-46d2-b8a6-087a144aa9c2",
+          "profile": {
+            "dob": "1992-08-21",
+            "name": "Mandy Durham",
+            "about": "Sunt pariatur pariatur voluptate irure officia dolor fugiat. Voluptate aliqua cupidatat velit consequat proident adipisicing do.",
+            "address": "97 Indiana Place, Northridge, Montana",
+            "company": "Zizzle",
+            "location": {
+              "lat": -28.623783,
+              "long": 44.274655
+            }
+          },
+          "username": "mandy92",
+          "createdAt": "2010-04-26T05:11:56.645Z",
+          "updatedAt": "2010-04-27T05:11:56.645Z"
+        },
+        {
+          "id": "6597dc6c28f38812e23e7be7",
+          "email": "schwartz_wooten@newcube.ck",
+          "roles": [
+            "member"
+          ],
+          "apiKey": "eec54d73-3767-42a2-9ab5-08ede5a9e1fe",
+          "profile": {
+            "dob": "1989-10-27",
+            "name": "Schwartz Wooten",
+            "about": "Id nostrud consectetur amet consequat eu consequat aliqua et aute dolor quis. Consectetur occaecat est nostrud officia.",
+            "address": "20 Krier Place, Chesapeake, West Virginia",
+            "company": "Newcube",
+            "location": {
+              "lat": -8.603228,
+              "long": 130.336191
+            }
+          },
+          "username": "schwartz89",
+          "createdAt": "2012-11-02T04:59:15.813Z",
+          "updatedAt": "2012-11-03T04:59:15.813Z"
+        },
+        {
+          "id": "6597dc6cb92bdc55b9825c31",
+          "email": "simone_hyde@xoggle.help",
+          "roles": [
+            "guest",
+            "member"
+          ],
+          "apiKey": "d478610e-7b59-4671-8a90-1716b8c13f2e",
+          "profile": {
+            "dob": "1992-03-24",
+            "name": "Simone Hyde",
+            "about": "Consequat minim excepteur nostrud deserunt fugiat excepteur. Fugiat aute laborum pariatur dolore est occaecat exercitation in in nisi occaecat.",
+            "address": "97 Graham Avenue, Fairforest, New Hampshire",
+            "company": "Xoggle",
+            "location": {
+              "lat": -9.502059,
+              "long": 11.327466
+            }
+          },
+          "username": "simone92",
+          "createdAt": "2014-11-25T03:13:54.270Z",
+          "updatedAt": "2014-11-26T03:13:54.270Z"
+        },
+        {
+          "id": "6597dc6ce6d89b438d883128",
+          "email": "butler_martin@xeronk.org",
+          "roles": [
+            "owner"
+          ],
+          "apiKey": "d9059c20-f747-45d6-8ede-ad50c7c5cc8b",
+          "profile": {
+            "dob": "1992-08-06",
+            "name": "Butler Martin",
+            "about": "Mollit adipisicing aliquip excepteur laboris eiusmod in occaecat eiusmod pariatur. Ex tempor tempor ad deserunt est mollit dolor sit voluptate velit tempor reprehenderit nulla.",
+            "address": "2 Dekalb Avenue, Clarktown, Kansas",
+            "company": "Xeronk",
+            "location": {
+              "lat": -71.604783,
+              "long": -67.550137
+            }
+          },
+          "username": "butler92",
+          "createdAt": "2012-02-05T07:28:42.603Z",
+          "updatedAt": "2012-02-06T07:28:42.603Z"
+        },
+        {
+          "id": "6597dc6c28426c1312a122c0",
+          "email": "megan_gonzalez@tri@tribalog.cc",
+          "roles": [
+            "guest",
+            "owner"
+          ],
+          "apiKey": "d220eb82-138e-4723-842c-552b50681bb8",
+          "profile": {
+            "dob": "1992-11-22",
+            "name": "Megan Gonzalez",
+            "about": "Nostrud ullamco adipisicing enim veniam excepteur culpa sit consequat. Et nulla duis nisi et et elit ut non do anim nisi irure sunt mollit.",
+            "address": "60 Driggs Avenue, Cetronia, Mississippi",
+            "company": "Tri@Tribalog",
+            "location": {
+              "lat": 52.586837,
+              "long": 33.049127
+            }
+          },
+          "username": "megan92",
+          "createdAt": "2011-08-10T17:17:01.589Z",
+          "updatedAt": "2011-08-11T17:17:01.589Z"
+        }
+      ]
