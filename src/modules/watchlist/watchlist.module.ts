7cfd9c0990a32d53ade2eef0f29e34ffc118fb3a
Екатерина Петрова <ekaterinap@MacBook-Air-Ekaterina.local>
2024-03-10 17:35:53 +0400

    add watchlist get

--- src/modules/watchlist/watchlist.module.ts
+++ src/modules/watchlist/watchlist.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { WatchlistController } from './watchlist.controller';
 import { WatchlistService } from './watchlist.service';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { Watchlist } from './models/watchlist.model';
+import { SequelizeModule } from "@nestjs/sequelize";
+import { Watchlist } from "./models/watchlist.model";
 
 @Module({
   imports: [SequelizeModule.forFeature([Watchlist])],
