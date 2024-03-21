2be460ce3e2f01656085f97c28482f7e6797ceca
Екатерина Петрова <ekaterinap@MacBook-Air-Ekaterina.local>
2024-03-21 10:51:34 +0400

    refactoring app

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,28 +1,36 @@
-import { Body, Controller, Delete, Patch, Req, UseGuards,} from '@nestjs/common';
-import { UserService } from './users.service';
-import { UpdateUserDTO } from './dto';
-import { JwtAuthGuard } from 'src/guards/jwt-guard';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Delete, Patch, Req, UseGuards } from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { UpdatePasswordDTO, UpdateUserDTO } from "./dto";
+import { JwtAuthGuard } from "../../guards/jwt-guard";
+import { ApiResponse, ApiTags } from "@nestjs/swagger";
 
-
-@Controller('users')
+@Controller("users")
 export class UsersController {
-    constructor(private readonly userService: UserService) {}
-    
+    constructor(private readonly userService: UsersService) {
+    }
+
     @ApiTags("API")
     @ApiResponse({status: 200, type: UpdateUserDTO})
     @UseGuards(JwtAuthGuard)
     @Patch()
     updateUser(@Body() updateDto: UpdateUserDTO, @Req() request): Promise<UpdateUserDTO> {
         const user = request.user
-        return this.userService.updateUser(user.email, updateDto)
+        return this.userService.updateUser(user.id, updateDto)
+    }
+    
+    @ApiTags("API")
+    @ApiResponse({status: 200})
+    @UseGuards(JwtAuthGuard)
+    @Patch('change-password')
+    updatePassword(@Body() updatePasswordDto: UpdatePasswordDTO, @Req() request): Promise<any> {
+        const user = request.user
+        return this.userService.updatePassword(user.id, updatePasswordDto)
     }
 
     @UseGuards(JwtAuthGuard)
     @Delete()
-    deleteUser (@Req() request): Promise<boolean> {
+    deleteUser (@Req() request): Promise<boolean>  {
         const user = request.user
-        return this.userService.deleteUser(user.email)
+        return this.userService.deleteUser(user.id)
     }
-
 }
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
-import { UserService } from './users.service';
+import { UsersService } from './users.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './models/user.model';
 import { TokenModule } from '../token/token.module';
@@ -8,7 +8,7 @@ import { TokenModule } from '../token/token.module';
 @Module({
   imports: [SequelizeModule.forFeature([User]), TokenModule],
   controllers: [UsersController],
-  providers: [UserService],
-  exports: [UserService]
+  providers: [UsersService],
+  exports: [UsersService]
 })
 export class UsersModule {}
