aa9d71e46db4c5ec80f702bd03e25ebdcc1d9a94
Екатерина Петрова <ekaterinap@MacBook-Air-Ekaterina.local>
2024-04-17 12:04:24 +0400

    create order logic

---
+++ src/modules/order/order.controller.ts
@@ -0,0 +1,42 @@
+import { Body, Controller, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { OrderService } from './order.service';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { OrderDTO, UpdateOrderDTO } from './dto';
+import { JwtAuthGuard } from 'src/guards/jwt-guard';
+import { RolesGuard } from 'src/guards/role-guard';
+import { Roles } from 'src/common/decorator';
+import { UserRole } from '../users/models/user.model';
+import { AllOrderResponse } from './response';
+
+@Controller('order')
+export class OrderController {
+    constructor(private readonly orderService: OrderService) {}
+
+    @ApiTags("API")
+    @ApiResponse({status: 201, type: OrderDTO })
+    @UseGuards(JwtAuthGuard)
+    @Post('create')
+    order(@Body() createDto: OrderDTO): Promise <OrderDTO> {
+        return this.orderService.createOrder(createDto)
+    }
+
+    @ApiTags("API")
+    @ApiResponse({status: 200, type: AllOrderResponse })
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(UserRole.ADMIN)
+    @Get('get')
+    async getOrder(): Promise <AllOrderResponse> {
+        const order = await this.orderService.publicOrder();
+        return order;
+    }
+
+    @ApiTags("API")
+    @ApiResponse({ status: 200, type: UpdateOrderDTO })
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(UserRole.ADMIN)
+    @Patch('update/:id')
+    async updateOrder(@Param('id') id: number, @Body() updateDto: UpdateOrderDTO): Promise<UpdateOrderDTO> {
+        const updatedOrder = await this.orderService.updateOrder(id, updateDto)
+        return updatedOrder;
+    }
+}
